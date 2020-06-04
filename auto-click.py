4bb0bed19d5fc52d126d453e8474df0a26ddf1aa
unknown <nikossoftwaredev@gmail.com>
2020-06-04 23:38:32 +0300

    Almost

--- auto-click.py
+++ auto-click.py
@@ -18,75 +18,53 @@ while True:
     elif keyboard.is_pressed('a'):
         phase = 11
 
-
     if phase == 1:
         pyautogui.click(x=mouseX, y=mouseY)        
         pyautogui.mouseDown()
-        phase = 2            
-    elif phase == 2 :
-        im2 = ImageGrab.grab(bbox=(999,802,1001,803)) #formula
-        rgb1 = im2.convert('RGB')
-        r2, g2, b2 = rgb1.getpixel((0, 0))
-        if  r2 <= 10 and g2 < 10 and b2 <10 :
-            pyautogui.mouseUp()        
-            phase = 3
-    elif phase == 3:
-        p3 = ImageGrab.grab(bbox=(964,968,965,969)) #formula
-        rgb3 =p3.convert('RGB')
-        r3, g3, b3 = rgb3.getpixel((0, 0))
-        if  r2 != 255 and g2 != 255 and b3 != 255:
-            pyautogui.click(x=mouseX, y=mouseY,clicks=6, interval=0.1)
-            phase = 4
+        phase = 2           
+    elif phase == 2 :        
+        sleep(0.80) #time to stop the formula
+        pyautogui.mouseUp()        
+        phase = 3
+    elif phase == 3:    
+        sleep(0.60)  #time to wait for six clicks
+        pyautogui.click(x=mouseX, y=mouseY,clicks=6, interval=0.001)
+        phase = 4
     elif phase == 4:  
-        im5 = ImageGrab.grab(bbox=(740,968,741,969)) #led down 1
-        rgb5 = im5.convert('RGB')
-        r5, g5, b5 = rgb5.getpixel((0, 0))
-        if  r5 == 255 and g5 == 255 and b5 == 255: 
-            sleep(0.5)    
-            pyautogui.dragTo(mouseX, mouseY - 300, button='left')            
-            pyautogui.mouseDown()            
-            phase = 5
-    elif phase == 5:
-        im1 = ImageGrab.grab(bbox=(1012,799,1025,814)) #led up 1
-        rgb1 = im1.convert('RGB')
-        r1, g1, b1 = rgb1.getpixel((5, 5))
-        if  r1 == 84 and g1 == 216 and b1 == 90:
-            pyautogui.mouseUp()                           
-            phase = 6
-    elif phase == 6:            
-        exp = ImageGrab.grab(bbox=(798,968,799,969)) #e3airetikos
-        rgb1 = exp.convert('RGB')
-        r1, g1, b1 = rgb1.getpixel((0, 0))
-        if r1 != 255 and g1 != 255 and b1 != 255:                 
-            pyautogui.dragTo(mouseX, mouseY, button='left')                                     
-            phase = 7
-    elif phase == 7:         
-        im5 = ImageGrab.grab(bbox=(745,970,746,971)) #led down 2
-        rgb5 = im5.convert('RGB')
-        r5, g5, b5 = rgb5.getpixel((0, 0))
-        if  r5 == 255 and g5 == 255 and b5 == 255: 
-            sleep(0.75)    
-            pyautogui.dragTo(mouseX, mouseY - 300, button='left')            
+        sleep(0.5)   #time to wait for the 1st slide up           
+        pyautogui.dragTo(mouseX, mouseY - 101, button='left')                  
+        pyautogui.mouseDown()           
+        phase = 5
+    elif phase == 5: 
+        sleep(0.75)    
+        pyautogui.mouseUp()  
+        pyautogui.dragTo(mouseX, mouseY, button='left')   
+        phase = 7
+    elif phase == 7:           
+            pyautogui.dragTo(mouseX, mouseY - 101, button='left')  
+            pyautogui.dragTo(mouseX, mouseY, button='left') 
+            pyautogui.dragTo(mouseX, mouseY - 101, button='left') 
+            pyautogui.dragTo(mouseX, mouseY, button='left') 
+            pyautogui.dragTo(mouseX, mouseY - 101, button='left')         
+            pyautogui.dragTo(mouseX, mouseY, button='left') 
+            pyautogui.dragTo(mouseX, mouseY - 101, button='left')    
+            pyautogui.dragTo(mouseX, mouseY, button='left') 
+            pyautogui.dragTo(mouseX, mouseY - 101, button='left')                    
             pyautogui.mouseDown()   
-            phase = 8               
+            phase = 8             
     elif phase == 8:
-        im1 = ImageGrab.grab(bbox=(1012,799,1025,814)) #led up 1
-        rgb1 = im1.convert('RGB')
-        r1, g1, b1 = rgb1.getpixel((5, 5))
-        if  r1 == 84 and g1 == 216 and b1 == 90:
-            pyautogui.mouseUp()       
-            pyautogui.dragTo(mouseX, mouseY, button='left')     
-            phase = 9
-    elif phase == 9:
-        pyautogui.click(x=mouseX, y=mouseY,clicks=6, interval=0.1)
-        phase = 10
-    elif phase == 10:
-        pyautogui.dragTo(mouseX + 300, mouseY, button='left')  
+        sleep(0.70) #time to release when green
+        pyautogui.mouseUp()     
+        pyautogui.dragTo(mouseX, mouseY, button='left')                 
+        pyautogui.click(x=mouseX, y=mouseY,clicks=6, interval=0.001)       
+        pyautogui.dragTo(mouseX + 101, mouseY, button='left')  
         pyautogui.moveTo(mouseX,mouseY)  
-        pyautogui.dragTo(mouseX + 300, mouseY, button='left')  
+        pyautogui.dragTo(mouseX + 101, mouseY, button='left')  
+        pyautogui.moveTo(mouseX,mouseY)   
+        pyautogui.dragTo(mouseX + 101, mouseY, button='left')  
         pyautogui.moveTo(mouseX,mouseY) 
-        pyautogui.dragTo(mouseX + 300, mouseY, button='left')  
-        pyautogui.moveTo(mouseX,mouseY) 
+        pyautogui.dragTo(mouseX + 101, mouseY, button='left')  
+        pyautogui.moveTo(mouseX,mouseY)                
         phase = 11
     elif phase == 11:
         break             
